c0be8de48635947bc4dc650b2756cf828936cce0
tyramisso <tainhafrederico@hotmail.com>
2025-04-07 23:45:24 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '75bded28-e894-47d3-a123-6a0ca796f094';
+export const MODAL_CLOSE_SEC = '2.5';
